9b75fbbc8a9beaa546c97e9631ed241607daca0b
Takio W <oswini@Oswins-MacBook-Pro.local>
2018-06-13 15:25:21 -0400

    user service java added

---
+++ com/example/his1stapp/services/UserService.java
@@ -0,0 +1,35 @@
+package com.example.his1stapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.his1stapp.repositories.UserRepository;
+import com.example.his1stapp.models.User;
+
+@RestController
+public class UserService {
+	   @Autowired
+	   UserRepository repository;
+	   
+	   @DeleteMapping("/api/user/{userId}")
+	     public void deleteUser (@PathVariable("userId") int id) {
+		   repository.deleteById(id);
+	   }
+	   
+	   @PostMapping("/api/user")
+	   public User createUser(@RequestBody User user) {
+		   return repository.save(user);
+	   }
+	   
+	   @GetMapping("/api/user")
+       public List <User> findAllUsers(){
+    	   return (List<User>) repository.findAll();
+}
+}
